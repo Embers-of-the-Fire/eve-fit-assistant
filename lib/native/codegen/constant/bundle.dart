12de55ac2dff46379c47eb91c4ebc942c349cfd0
Embers-of-the-Fire <stellarishs@163.com>
2025-02-16 14:24:51 +0800

    add: create bundle for 0.3.0

--- lib/native/codegen/constant/bundle.dart
+++ lib/native/codegen/constant/bundle.dart
@@ -1,4 +1,4 @@
 import 'package:eve_fit_assistant/storage/static/storage.dart';
 
 /// Bundled static data version
-const StaticVersionInfo dataVersion = StaticVersionInfo(createTime: 1739625664);
+const StaticVersionInfo dataVersion = StaticVersionInfo(createTime: 1739686933);
