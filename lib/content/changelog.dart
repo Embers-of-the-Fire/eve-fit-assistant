b6e87538521ca93ee6a3ac814074c048dffd78de
Embers-of-the-Fire <stellarishs@163.com>
2025-02-22 11:24:16 +0800

    add: update changelog

--- lib/content/changelog.dart
+++ lib/content/changelog.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 final List<Content> changelogContents = Content.of([
   Content(
       icon: Icons.update,
+      title: '更新日志 EFA v0.8.0',
+      path: 'content/changelog/v0.8.0.md',
+      time: DateTime(2025, 2, 22, 11, 17)),
+  Content(
+      icon: Icons.update,
       title: '更新日志 EFA v0.7.0',
       path: 'content/changelog/v0.7.0.md',
       time: DateTime(2025, 2, 20, 21, 56)),
