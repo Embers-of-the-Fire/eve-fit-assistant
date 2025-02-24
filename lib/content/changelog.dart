6eefacb434aa9a001eb41cc7a28c9034c2889927
Embers-of-the-Fire <stellarishs@163.com>
2025-02-24 21:31:45 +0800

    add: update changelog

--- lib/content/changelog.dart
+++ lib/content/changelog.dart
@@ -6,7 +6,7 @@ final List<Content> changelogContents = Content.of([
       icon: Icons.update,
       title: '更新日志 EFA v0.9.0',
       path: 'content/changelog/v0.9.0.md',
-      time: DateTime(2025, 2, 22, 17, 00)),
+      time: DateTime(2025, 2, 24, 21, 31)),
   Content(
       icon: Icons.update,
       title: '更新日志 EFA v0.8.0',
