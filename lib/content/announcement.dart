8397de2dd68e414604be02817ffbd87e41d2ce80
Embers-of-the-Fire <stellarishs@163.com>
2025-03-01 19:38:37 +0800

    add: add docs for beta test

--- lib/content/announcement.dart
+++ lib/content/announcement.dart
@@ -5,6 +5,11 @@ import 'package:flutter/material.dart';
 final List<Content> announcementContents = Content.of([
   Content(
       icon: Icons.info_outline,
+      title: '关于 BETA 测试的一些须知和信息',
+      path: 'content/announcement/about-beta-test-version.md',
+      time: DateTime(2025, 3, 1, 19, 37)),
+  Content(
+      icon: Icons.info_outline,
       title: '关于测试版的一些须知和信息',
       path: 'content/announcement/about-test-version.md',
       time: DateTime(2025, 2, 14, 16, 18)),
