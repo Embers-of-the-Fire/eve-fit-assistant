7628f2a3d4ae0acdf437b41f5a901fc43eb62380
Embers-of-the-Fire <stellarishs@163.com>
2025-03-07 09:43:51 +0800

    version: bump app version and update changelog

--- lib/content/changelog.dart
+++ lib/content/changelog.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 final List<Content> changelogContents = Content.of([
   Content(
       icon: Icons.update,
+      title: '更新日志 EFA v0.15.0',
+      path: 'content/changelog/v0.15.0.md',
+      time: DateTime(2025, 3, 7, 9, 43)),
+  Content(
+      icon: Icons.update,
       title: '更新日志 EFA v0.14.0',
       path: 'content/changelog/v0.14.0.md',
       time: DateTime(2025, 3, 2, 15, 25)),
