3ddbad65d147f4248283d71aa4741caa4bfa8723
Embers-of-the-Fire <stellarishs@163.com>
2025-10-08 22:28:40 +0800

    rm: remove generated l10n

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,4 @@
-import 'package:eve_fit_assistant/l10n/app_localizations.dart';
+import 'package:eve_fit_assistant/data/l10n/app_localizations.dart';
 import 'package:flutter/widgets.dart';
 
 part 'utils/l10n.dart';
