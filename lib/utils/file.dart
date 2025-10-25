4018b35fdd4da4c6eda6d83adb36b4e68081cb22
Embers-of-the-Fire <stellarishs@163.com>
2025-10-25 23:13:23 +0800

    fix: fix file copy(recursive)

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -1,5 +1,8 @@
 import "dart:io";
 
+import "package:flutter/cupertino.dart";
+import "package:path/path.dart" as p;
+
 Future<void> copyRecursive(Directory source, Directory target) async {
   if (!source.existsSync()) {
     throw Exception("Source directory does not exist: ${source.path}");
@@ -9,17 +12,34 @@ Future<void> copyRecursive(Directory source, Directory target) async {
   }
 
   await for (final entity in source.list(followLinks: false)) {
-    final name = entity.uri.pathSegments.isNotEmpty ? entity.uri.pathSegments.last : entity.path;
-    final destPath = "${target.path}${Platform.pathSeparator}$name";
+    // compute the path of the entity relative to the source
+    final relativeName = p.relative(entity.path, from: source.path);
+    final destPath = p.join(target.path, relativeName);
+    debugPrint("Dest Path: $destPath");
 
-    switch (entity) {
-      case final File file:
-        await file.copy(destPath);
-      case final Directory dir:
-        await copyRecursive(dir, Directory(destPath));
-      case final Link link:
-        final target = await link.target();
-        await Link(destPath).create(target);
+    if (entity is File) {
+      final destFile = File(destPath);
+      final parent = destFile.parent;
+      if (!parent.existsSync()) {
+        await parent.create(recursive: true);
+      }
+      if (destFile.existsSync()) {
+        await destFile.delete();
+      }
+      await entity.copy(destPath);
+    } else if (entity is Directory) {
+      final destDir = Directory(destPath);
+      if (!destDir.existsSync()) {
+        await destDir.create(recursive: true);
+      }
+      await copyRecursive(entity, destDir);
+    } else if (entity is Link) {
+      final linkTarget = await entity.target();
+      final parent = Directory(p.dirname(destPath));
+      if (!parent.existsSync()) {
+        await parent.create(recursive: true);
+      }
+      await Link(destPath).create(linkTarget);
     }
   }
 }
