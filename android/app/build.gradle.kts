7a9a7827a9f938501cd3b6dd1e88feff4883d2b9
Embers-of-the-Fire <stellarishs@163.com>
2026-02-27 19:44:29 +0800

    rel: 20260227-serenity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.efa.eve_fit_assistant"
+    namespace = "dev.efa.eve_fit_assistant_serenity"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.efa.eve_fit_assistant"
+        applicationId = "dev.efa.eve_fit_assistant_serenity"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
