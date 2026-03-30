c096264b80b44ed48416e4dcd038b73f0de8f971
Embers-of-the-Fire <stellarishs@163.com>
2026-03-30 08:08:22 +0800

    rel: 20260330-tranquility

--- lib/native/codegen/constant/character.dart
+++ lib/native/codegen/constant/character.dart
@@ -2,11 +2,11 @@
 /// - ID: predefined-level-alpha-max
 const String predefinedLevelAlphaMax = 'predefined-level-alpha-max';
 
-/// - Name: All 5
-/// - ID: predefined-level-all5
-const String predefinedLevelAll5 = 'predefined-level-all5';
-
 /// - Name: All 0
 /// - ID: predefined-level-all0
 const String predefinedLevelAll0 = 'predefined-level-all0';
+
+/// - Name: All 5
+/// - ID: predefined-level-all5
+const String predefinedLevelAll5 = 'predefined-level-all5';
 
