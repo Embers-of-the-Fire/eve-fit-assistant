1e51aed1f559664b610bd81eb173be8f79504dd9
Embers-of-the-Fire <stellarishs@163.com>
2026-04-15 18:34:21 +0800

    feat: enhance workspace release shortcuts and refine shell navigation (#43)
    Closes #23 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Navigation Updates**
      * Removed Character page from the app
      * Reindexed navigation items
    
    * **Workspace Changes**
      * Updated workspace shortcuts with localized settings options
      * Added quick access to Bundle Manager and App Settings
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Signed-off-by: Embers-of-the-Fire <stellarishs@163.com>

--- lib/pages/view.dart
+++ lib/pages/view.dart
@@ -4,7 +4,6 @@ import "dart:math" as math;
 
 import "package:auto_route/auto_route.dart";
 import "package:eve_fit_assistant/features/fit_io/import_dialog.dart";
-import "package:eve_fit_assistant/pages/character/page.dart";
 import "package:eve_fit_assistant/pages/fit-list/page.dart";
 import "package:eve_fit_assistant/pages/router.dart";
 import "package:eve_fit_assistant/pages/setting/page.dart";
@@ -39,14 +38,12 @@ class _FrontPageState extends State<FrontPage> {
     final pageTitles = [
       loc.frontPageTitleWorkspace,
       loc.frontPageTitleFitList,
-      loc.frontPageTitleCharacter,
       loc.frontPageTitleSetting,
     ];
-    const pages = <Widget>[WorkspacePage(), FitListPage(), CharacterPage(), SettingPage()];
+    const pages = <Widget>[WorkspacePage(), FitListPage(), SettingPage()];
     const pageIcons = <IconData>[
       Icons.dashboard_rounded,
       Icons.list_alt_rounded,
-      Icons.account_circle,
       Icons.settings_rounded,
     ];
 
@@ -117,6 +114,15 @@ class _FrontPageState extends State<FrontPage> {
                               .then((_) => setState(() => _currentIndex = idx)),
                         ),
                         const Expanded(child: SizedBox()),
+                        const Expanded(child: SizedBox()),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Expanded(child: SizedBox()),
                         _NavItem(
                           index: 1,
                           icon: pageIcons[1],
@@ -131,33 +137,11 @@ class _FrontPageState extends State<FrontPage> {
                               .then((_) => setState(() => _currentIndex = idx)),
                         ),
                         const Expanded(child: SizedBox()),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Expanded(child: SizedBox()),
                         _NavItem(
                           index: 2,
                           icon: pageIcons[2],
                           label: pageTitles[2],
                           selected: _currentIndex == 2,
-                          onTap: (idx) => _pageController
-                              .animateToPage(
-                                idx,
-                                duration: const Duration(milliseconds: 300),
-                                curve: Curves.easeInOut,
-                              )
-                              .then((_) => setState(() => _currentIndex = idx)),
-                        ),
-                        const Expanded(child: SizedBox()),
-                        _NavItem(
-                          index: 3,
-                          icon: pageIcons[3],
-                          label: pageTitles[3],
-                          selected: _currentIndex == 3,
                           onTap: (idx) => _pageController
                               .animateToPage(
                                 idx,
