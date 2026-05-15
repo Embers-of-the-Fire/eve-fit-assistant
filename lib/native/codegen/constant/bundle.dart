6cca16007b4cc85ae49e161761c4bfc4a5db62da
Embers-of-the-Fire <stellarishs@163.com>
2026-05-15 13:38:13 +0800

    rel: 20260515-serenity
    Signed-off-by: Embers-of-the-Fire <stellarishs@163.com>

--- lib/native/codegen/constant/bundle.dart
+++ lib/native/codegen/constant/bundle.dart
@@ -1,4 +1,4 @@
 import 'package:eve_fit_assistant/storage/static/storage.dart';
 
 /// Bundled static data version
-const StaticVersionInfo bundledStaticVersion = StaticVersionInfo(createTime: 1777644825);
+const StaticVersionInfo bundledStaticVersion = StaticVersionInfo(createTime: 1778823078);
