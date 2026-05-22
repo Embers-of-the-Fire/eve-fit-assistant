5abcfdf424a61eb78ce538470968b808304255d1
Embers-of-the-Fire <stellarishs@163.com>
2026-05-22 12:41:10 +0800

    rel: 20260521-singularity
    Signed-off-by: Embers-of-the-Fire <stellarishs@163.com>

--- lib/native/codegen/constant/character.dart
+++ lib/native/codegen/constant/character.dart
@@ -1,7 +1,3 @@
-/// - Name: All 5
-/// - ID: predefined-level-all5
-const String predefinedLevelAll5 = 'predefined-level-all5';
-
 /// - Name: Alpha Max
 /// - ID: predefined-level-alpha-max
 const String predefinedLevelAlphaMax = 'predefined-level-alpha-max';
@@ -9,4 +5,8 @@ const String predefinedLevelAlphaMax = 'predefined-level-alpha-max';
 /// - Name: All 0
 /// - ID: predefined-level-all0
 const String predefinedLevelAll0 = 'predefined-level-all0';
+
+/// - Name: All 5
+/// - ID: predefined-level-all5
+const String predefinedLevelAll5 = 'predefined-level-all5';
 
