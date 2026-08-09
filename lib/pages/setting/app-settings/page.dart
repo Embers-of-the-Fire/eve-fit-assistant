6bccad5e130b97d0e8a3ecf9dfebc1ae77847f66
Embers-of-the-Fire <stellarishs@163.com>
2026-08-09 22:04:50 +0800

    feat(settings): add Windows-only storage location tile
    Lets the user pick a new storage root, migrates the app-owned storage
    directories to it, persists the bootstrap preference, and restarts the
    app. Includes a restore-default action.
    
    Assisted-by: kimi:k3
    Signed-off-by: Embers-of-the-Fire <stellarishs@163.com>

--- lib/pages/setting/app-settings/page.dart
+++ lib/pages/setting/app-settings/page.dart
@@ -1,3 +1,5 @@
+import "dart:async";
+
 import "package:auto_route/auto_route.dart";
 import "package:eve_fit_assistant/components/dialog/confirm_dialog.dart";
 import "package:eve_fit_assistant/components/dialog/info_dialog.dart";
@@ -5,14 +7,20 @@ import "package:eve_fit_assistant/components/layout.dart";
 import "package:eve_fit_assistant/components/list/config_list.dart";
 import "package:eve_fit_assistant/components/list/dropdown_list_tile.dart";
 import "package:eve_fit_assistant/config/list_tile_anti_scroll.dart";
+import "package:eve_fit_assistant/config/loading.dart";
 import "package:eve_fit_assistant/config/locale.dart" show Locale;
+import "package:eve_fit_assistant/config/paths.dart";
+import "package:eve_fit_assistant/config/storage_root.dart";
 import "package:eve_fit_assistant/config/type_list.dart";
 import "package:eve_fit_assistant/features/app_update/platform/update_platform.dart";
 import "package:eve_fit_assistant/storage/setting/setting.dart";
 import "package:eve_fit_assistant/utils/context.dart";
+import "package:file_picker/file_picker.dart";
 import "package:flutter/foundation.dart" show kIsWeb;
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:path/path.dart" as p;
+import "package:restart_app/restart_app.dart";
 
 part "anti_scroll.dart";
 part "font_scale.dart";
@@ -20,6 +28,7 @@ part "impact_warning.dart";
 part "locale.dart";
 part "market_price.dart";
 part "select_list.dart";
+part "storage_root.dart";
 part "update_strategy.dart";
 
 @RoutePage()
@@ -35,6 +44,12 @@ class AppSettingsPage extends ConsumerWidget {
         const ConfigListTile.custom(LocaleTile()),
         const ConfigListTile.custom(FontScaleTile()),
         const ConfigListTile.custom(ListTileAntiScrollTile()),
+        // The configured storage root is honored on every native platform,
+        // but only Windows exposes editing.
+        if (storageRootEditable) ...[
+          ConfigListTile.title(context.l10n.appSettingsPageSectionStorage),
+          const ConfigListTile.custom(StorageRootTile()),
+        ],
         ConfigListTile.title(context.l10n.appSettingsPageSectionSelectList),
         const ConfigListTile.custom(ShipCreateListTile()),
         const ConfigListTile.custom(ListReturnBehaviorTile()),
